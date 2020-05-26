99fc7ccc5be35670dcf799c39174ec9218f76f63
mstjepan28 <markovcics@gmail.com>
2020-05-26 13:35:32 +0200

    Import/Export files & file tests

--- src/parse_file.js
+++ src/parse_file.js
@@ -1,19 +1,45 @@
+// READ THE TEXT FILE //
 function read_file(){
     const fs = require('fs')
     const filePath = String.raw`C:\Users\Stjepan\Desktop\tdd-demo-master\src\Text\long.txt`;
     
-    return fs.readFileSync(filePath, 'utf8').split(/[\s,!.:]+/);
+    return fs.readFileSync(filePath, 'utf8').split(/[\s\n",!.?()@%€$:*+-]/).filter(element => element !== '');
 }
 
-function build_map(text){
-    return text[0]
+// EXPORT AS JSON //
+function export_as_json(final_map){
+    const fs = require('fs');
+    
+    fs.writeFile("./Parsed/final_map.json", JSON.stringify(final_map), 'utf8', ()=>{console.log("map saved!")})
 }
 
+
+// BUILD FINAL // 
+function build_map(single, double, triple){
+    return {
+        1: single,
+        2: double,
+        3: triple
+    }
+}
+
+// MAIN FUNCTION
 function parse_file(){
     let text = read_file()
-    return build_map(text)
+
+    const single = {}//map_single(text, {});
+    const double = {}//map_double(text, {});
+    const triple = {}//map_triple(text, {});
+
+    let final = build_map(single, double, triple);
+    export_as_json(final);
 }
 
 parse_file()
 
-module.exports = parse_file;
+module.exports = {
+    parse_file: parse_file,
+    read_file: read_file,
+    build_map: build_map,
+    export_as_json: export_as_json,
+};
--- src/test/file_parsing.spec.js
+++ src/test/file_parsing.spec.js
@@ -1,12 +1,48 @@
 var assert = require('assert');
-const parse_file = require("../parse_file");
+const parse_file = require("../parse_file.js");
 
-describe('Parsiranje long.txt', function() {
+const parse = parse_file.parse_file;
+const read_file = parse_file.read_file;
+const build_map = parse_file.build_map;
+const export_map = parse_file.export_as_json;
+
+
+describe('parse_file funkcija', function() {
     it('Funkcija postoji', function () {
-        parse_file();
+        parse();
     });
-    it("Razdvajanje rijeci u fileu", function() {
-        assert.equal(parse_file(), "Hello"); 
-     });
+});
+
+describe('read_file funkcija', function() {
+    it('read_file vraca polje', function () {
+        let result = read_file();
+        assert.equal(Array.isArray(result), Array.isArray([]) ); 
+    });
+    it('Prvi element polja je "Hello"', function () {
+        let result = read_file();
+        assert.equal(result[0], 'Hello'); 
+    });
+    it('Drugi element polja je "everyone"', function () {
+        let result = read_file();
+        assert.equal(result[1], 'everyone'); 
+    });
+});
+
+describe('build_map funkcija', function() {
+    it('funckija postoji', function () {
+        build_map();
+    });
+    it('Vracani rijecnik je validan / sadrzava prave podatke', function () {
+        let result = build_map({}, {}, {});
+        assert.notEqual(result['1'], undefined);
+        assert.notEqual(result['2'], undefined); 
+        assert.notEqual(result['3'], undefined); 
+    });
+});
+
+describe('export_as_json funkcija', function() {
+    it('funckija postoji', function () {
+        export_map();
+    });
 });
 
