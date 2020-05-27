9103432c2621e265df1c8dd274e4bd753bc78771
mstjepan28 <markovcics@gmail.com>
2020-05-27 14:15:07 +0200

    SuggestionTool+test & get_parsed+test refactoring

--- src/test/word_suggestion.spec.js
+++ src/test/word_suggestion.spec.js
@@ -1,9 +1,13 @@
 var assert = require('assert');
 
 const word_suggestion = require("../word_suggestion.js");
-const file = word_suggestion['file'];
 
+const file = word_suggestion['file'];
 const get_parsed_file = file.get_parsed_file;
+
+const suggestion = word_suggestion['suggestion'];
+const suggestionTool = suggestion.suggestionTool;
+
 
 describe('get_parsed_file funkcija', function() {
     it('get_parsed_file za len = 1 / single_map.json', function () {
@@ -20,6 +24,18 @@ describe('get_parsed_file funkcija', function() {
         let res = get_parsed_file(3)
         let expected =  { is: 1 };
         assert.equal(JSON.stringify(res["hello everyone this"]), JSON.stringify(expected));
+    });
+    it('get_parsed_file za len = 4 / undefined', function () {
+        let res = get_parsed_file(4)
+        let expected =  {};
+        assert.equal(JSON.stringify(res), JSON.stringify(expected));
+    });
+});
+
+describe('suggestionTool funkcija', function() {
+    it('suggestionTool za nepostojece podatke', function () {
+        let res = suggestionTool("a b c d e f g")
+        assert.equal(res, "No matches");
     });
 });
 
--- src/word_suggestion.js
+++ src/word_suggestion.js
@@ -1,17 +1,28 @@
 function get_parsed_file(len){
-    let file_map = {1: String.raw`\single_map.json`, 2: String.raw`\double_map.json`, 3: String.raw`\triple_map.json`}
-
+    let file_map = {1: String.raw`\single_map.json`, 2: String.raw`\double_map.json`, 3: String.raw`\triple_map.json`, get undefined(){return String.raw`\error_handler.json`} }
+    let num_map = {1:1, 2:2, 3:3} //Provjeravamo ako je len u dozvoljenom rasponu, ako nije vraca se undefined koji se obraduje dalje u file_map
+    
     const fs = require('fs')
-    const filePath = String.raw`C:\Users\Stjepan\Desktop\tdd-demo-master\src\Parsed` + file_map[len];
+    const filePath = String.raw`C:\Users\Stjepan\Desktop\tdd-demo-master\src\Parsed` + file_map[num_map[len]];
     
     return JSON.parse(fs.readFileSync(filePath, 'utf8'));
 }
 
-function suggestionTool(currentText){
-    let parsed_json = get_parsed_file(currentText.length)
-
+function get_suggestion(phrase){
+    
 }
-let single = get_parsed_file(1)
+
+function suggestionTool(phrase){
+    let type_handler = {
+        'phrase': null,
+        get undefined(){return "No matches"}, 
+        get object(){return get_suggestion(this.phrase)}
+    }
+    type_handler['phrase'] = phrase;
+    
+    let file = get_parsed_file(phrase.length)
+    return type_handler[typeof(file[phrase])]
+}
 
 //console.log(suggestionTool());
 //["I AM just", "I am often", "I am currently", "I am 90%"]
@@ -20,4 +31,7 @@ module.exports = {
     'file':{
         get_parsed_file: get_parsed_file,    
     },
+    'suggestion':{
+        suggestionTool: suggestionTool
+    }
 };
